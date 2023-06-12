3fda1a421e49dac8bc276a43b349ad62fd61e0c3
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-06-12 15:20:20 -0700

    Include Create and Read from database functionality.

--- app.js
+++ app.js
@@ -1,75 +1,96 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
-const _ = require("lodash")
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
+const _ = require("lodash");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+// Content for different pages
+const homeStartingContent = "...";
+const aboutContent = "...";
+const contactContent = "...";
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-app.get("/", function(req, res) {
-  res.render("home", {startingContent: homeStartingContent, posts: posts});
-})
-
-app.get("/about", function(req, res) {
-  res.render("about", {aboutContent: aboutContent});
-})
-
-app.get("/contact", function(req, res) {
-  res.render("contact", {contactContent: contactContent})
-})
-
-app.get("/compose", function(req, res) {
-  res.render("compose");
-})
-
-app.post("/compose", function(req, res) {
-  
-  const post = {
-    title: req.body.title,
-    body: req.body.body,
-  };
-
-  posts.push(post);
-  res.redirect("/")
+// Connect to the MongoDB database
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get("/posts/:title", function(req, res) {
-  
-  const requestedTitle = _.lowerCase(req.params.title)
-  posts.forEach(function(post) {
-    if (_.lowerCase(post.title) === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.body,
-      });
-      return true;
-    } 
-    console.log("NO Match Found!")
-    return false;
+// Define the posts schema and model
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+
+// Configure middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Home route
+app.get("/", async (req, res) => {
+  // Retrieve all posts from the database
+  const posts = await Post.find({});
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
+});
+
+// About route
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+// Contact route
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+// Compose route (render the compose form)
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+// Compose route (handle form submission)
+app.post("/compose", async (req, res) => {
+  // Create a new post object with data from the form
+  const post = new Post({
+    title: req.body.title,
+    body: req.body.body,
   });
-})
 
+  // Save the post to the database
+  await post.save();
+  res.redirect("/");
+});
 
+// Individual post route
+app.get("/posts/:_id", async (req, res) => {
+  try {
+    // Find the requested post by its ID
+    const requestedPost = await Post.findOne({ _id: req.params._id });
 
+    // Render the post template with the requested post data
+    res.render("post", {
+      title: requestedPost.title,
+      body: requestedPost.body,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Oops, something went wrong.");
+  }
+});
 
-
-
-
-
-
-
-
-app.listen(3000, function() {
+// Start the server
+app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
